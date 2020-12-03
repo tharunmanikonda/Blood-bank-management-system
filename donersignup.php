b79963ecd29ef81c760797ec436b937ede02a914
tharun <tharunmanikonda885@gmail.com>
2020-12-03 22:33:33 +0530

    added dbms signup files

---
+++ donersignup.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>doner table :</h1>
+    <form method="post">
+    <label for="D_NAME"> NAME:</label>
+    <input type="text" id="D_NAME" name="name">
+    <br>
+    <label for="D_AGE"> AGE:</label>
+    <input type="text" id="D_AGE" name="age">
+    <br>
+    <label for="PHONENO"> PHONE NO:</label>
+    <input type="text" id="D_PHONENO" name="phoneno">
+    <br>
+    <label for="D_EMAIL">EMAIL:</label>
+    <input type="text" id="D_EMAIL" name ="emailid" >
+    <br>
+    <label for="D_ADDRESS"> ADDRESS:</label>
+    <input type="text" name="address" id="D_ADDRESS">
+    <br>
+    <label for="D_AMOUNT">AMOUNT:</label>
+    <input type="text" name="amount" id="D_AMOUNT">
+    <br>
+    <input type="submit" value="submit" id="submit">
+    
+    </form>
+</body>
+</html>
