6297ea16b84bf0e89e7f33a069944b1a04d94ce9
tharun <tharunmanikonda885@gmail.com>
2020-12-05 14:05:29 +0530

    updated sigmup form

--- doctor.php
+++ doctor.php
@@ -1,3 +1,19 @@
+<?php
+include_once "pdo.php";
+
+if (isset($_POST['name'])&& isset($_POST['phoneno'])&& isset($_POST['email'])&& isset($_POST['address'])){
+$sql = "INSERT INTO doctor (NAME,PHONENO,EMAIL,address) VALUES (:name ,:pno,:email,:address)";
+    echo ("<pre>\n".$sql."\n</pre>\n");
+     $stmt = $pdo->prepare($sql);
+     $stmt->execute(array(
+        ':name' => $_POST['name'],
+        ':pno'=> $_POST['phoneno'],
+        ':email' => $_POST['email'],
+        ':address' =>$_POST['address']));
+
+}
+
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,17 +23,23 @@
 <body>
 <form method="post">
    <P> <label for="NAME"> NAME:</label>
-    <input type="text" id="NAME" name="name">
+    <input type="text" id="name" name="name">
     <br>
     </P>
     <P> <label for="PHONENO"> PHONE NO:</label>
-    <input type="text" id="PHONENO" name="PHONENO">
+    <input type="text" id="phoneno" name="phoneno">
     <br>
     </P>
-    <P> <label for="UNITS"> UNITS:</label>
-    <input type="text" id="UNITS" name="UNITS">
+    <P> <label for="email"> EMAIL:</label>
+    <input type="email" id="email" name="email">
     <br>
     </P>
+    <P> <label for="address"> ADDRESS:</label>
+    <input type="text" id="address" name="address">
+    <br>
+    </P>
+    <input type="submit" value ="add" id="submit">
+
 </form>
 </body>
 </html>
--- donersignup.php
+++ donersignup.php
@@ -2,12 +2,30 @@
 include_once "pdo.php";
 $stmt = $pdo->query("SELECT * FROM blood_group");
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_POST['name']) && isset($_POST['age']) && isset($_POST['phoneno']) && 
+isset($_POST['emailid']) && isset($_POST['address']) && isset($_POST['amount']) && isset($_POST['blood_id'])) {
+    $sql = "INSERT INTO doner (D_NAME,D_AGE, D_PHONENO, D_EMAIL, D_ADDRESS,D_AMOUNT,blood_id)
+    VALUES (:name,:age,:phoneno,:emailid,:address,:amount ,:blood_id)";
+    echo ("<pre>\n".$sql."\n</pre>\n");
+     $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':name' => $_POST['name'],
+        ':age'=> ($_POST['age']),
+        ':phoneno'=> ($_POST['phoneno']),
+        ':emailid' => $_POST['emailid'],
+        ':address' =>$_POST['address'],
+        ':amount' => $_POST['amount'],
+        ':blood_id'=>$_POST['blood_id']));
+
+}
 ?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
 <body>
 <h1>doner table :</h1>
@@ -30,16 +48,18 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <label for="D_AMOUNT">AMOUNT:</label>
     <input type="text" name="amount" id="D_AMOUNT">
     <br>
-    <input type="submit" value="submit" id="submit">
+    <label for="blood type">TYPE OF BLOOD:</label>
     <select name="blood group" id="blood_group">
     <?php
      foreach($rows as $row){
         echo "<option value=".$row['blood_id'].">";
         echo htmlentities($row['TYPE_OF_BLOOD']);
         echo "</option>";
+    
     }
     ?>
      </select>
+     <br>
     <input type="submit"value="submit">
     
 
